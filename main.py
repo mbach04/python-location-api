24bcf49eded656f1497384b619975d838c119d3d
Matthew Bach <matthewbach@macbook.matthewbach.dev>
2021-04-27 16:18:58 -0400

    add point json return

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+def gen_point():
+  x, y = uniform(-180,180), uniform(-90, 90)
+  return {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [x, y]
+    },
+    "properties": {
+      "name": "sample point"
+    }
+  }
+
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return gen_point()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
